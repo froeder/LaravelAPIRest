ce2f9932e7050557705c8dc85177a8bdc6e9310e
Jhonatan Froeder <froeder3@gmail.com>
2019-07-09 16:57:30 -0400

    Inclusão migrations

--- server/database/migrations/2019_07_09_184843_create_ratings_table.php
+++ server/database/migrations/2019_07_09_184843_create_ratings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBooksTable extends Migration
+class CreateRatingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateBooksTable extends Migration
      */
     public function up()
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('ratings', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('user_id');
-            $table->string('title');
-            $table->text('description');
+            $table->unsignedInteger('book_id');
+            $table->unsignedInteger('rating');
             $table->timestamps();
           });
     }
@@ -29,6 +29,6 @@ class CreateBooksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('books');
+        Schema::dropIfExists('ratings');
     }
 }
